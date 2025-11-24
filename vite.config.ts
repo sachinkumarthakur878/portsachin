829958dd0c5c131b28e4f0c94c23e348b0b0d0e9
sachin thakur <sachinkumarthakur878@gmail.com>
2025-11-25 00:38:47 +0530

    fixed vite base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,6 +27,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portsachin/", // <-- ⭐⭐ IMPORTANT ⭐⭐
+  
   server: {
     host: "::",
     port: 8080,
@@ -20,3 +44,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
